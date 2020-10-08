18524aa184203aa7a509cfbf5de0d35dbb192be3
Kaid22 <avdickaid@gmail.com>
2020-10-08 13:46:25 +0200

    commit

--- primer2/Form1.cs
+++ primer2/Form1.cs
@@ -16,5 +16,19 @@ namespace primer2
         {
             InitializeComponent();
         }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            int a = 50;
+            int x = e.X;
+            int y = e.Y;
+            Point A = new Point(x - a / 2, y- a / 2);
+            Point B = new Point(x + a / 2, y- a / 2);
+            Point C = new Point(x + a / 2, y+ a / 2);
+            Point D = new Point(x - a / 2, y+ a / 2);
+            Graphics g = CreateGraphics();
+            PointF[] arr = { A,B,C,D,A,D,B,C };
+            g.DrawPolygon(Pens.Red, arr);
+        }
     }
 }
